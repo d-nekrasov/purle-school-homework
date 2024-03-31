56f34ac6649a680cc5fd5ace413982d75f7b0c3b
Denis Nekrasov <den.nekrasov@icloud.com>
2024-03-31 19:34:54 +0300

    6.7. Домашнее задание - Таймеры

---
+++ js/timer.js
@@ -0,0 +1,41 @@
+"use strict"
+
+/**
+ * 1. [x] Создайте базовую HTML-структуру для отображения таймера.
+ * 2. [x] В JavaScript, определите дату окончания года.
+ * 3. [x] Используйте setInterval для создания цикла, который каждую секунду будет обновлять таймер.
+ * 4. [x] Рассчитайте разницу между текущим временем и концом года.
+ * 5. [x] Форматируйте оставшееся время в формате "XX месяцев, XX дней, XX часов, XX минут, XX секунд".
+ * 6. [x] Покажите отформатированное время в таймере, который виден в браузере.
+ * 7. [x] Обновляйте таймер каждую секунду, уменьшая время до конца года.
+ *
+ */
+
+
+const newYearDate = new Date('01.01.2025 00:00:00');
+
+
+
+function setNewYearTimer(end) {
+    const now = new Date();
+    const $timer = document.querySelector('#newYearTimer');
+
+    const diff = end.getTime() - now.getTime()
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000)
+    const month = Math.floor(days / 30)
+
+    $timer.innerHTML = `${month} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`;
+
+}
+
+
+
+const interval = setInterval(() => {
+
+    setNewYearTimer(newYearDate)
+
+}, 1000)

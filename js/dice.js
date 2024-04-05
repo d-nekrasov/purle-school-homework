699222bac170aec65acb44e75d865ba9978eab76
Denis Nekrasov <den.nekrasov@icloud.com>
2024-04-05 18:45:06 +0300

    fix throwDice function

--- js/dice.js
+++ js/dice.js
@@ -1,20 +1,29 @@
 /**
- * @param type {string} тип многогранника, который будет имитироваться броском. Поддерживаемые типы: D4, D6, D8, D10, D12, D16, D20.
- * @returns {number} возвращает случайное число в диапазоне возможных значений для выбранного многогранника. Например, для D6 это числа от 1 до 6 включительно.
+ * @param diceType {string} тип многогранника, который будет имитироваться броском. Поддерживаемые типы: D4, D6, D8, D10, D12, D16, D20.
+ * @returns {any} возвращает случайное число в диапазоне возможных значений для выбранного многогранника. Например, для D6 это числа от 1 до 6 включительно.
  */
-function throwDice(type){
+function throwDice(diceType= "D4"){
+
+    const dices = ['D4', 'D6', 'D8', 'D10', 'D12', 'D16', 'D20'];
+
+    if(!dices.includes(diceType)){
+        return false;
+    }
+
     const minValue = 1;
-    const maxValue = Number(type.slice(1));
+    const maxValue = Number(diceType.slice(1));
 
     return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue)
 }
 
 console.log('4.11. Домашнее задание - Расчёты и math');
-console.log('D4:', throwDice('D4'))
-console.log('D6:', throwDice('D6'))
-console.log('D8:', throwDice('D8'))
-console.log('D10:', throwDice('D10'))
-console.log('D12:', throwDice('D12'))
-console.log('D16:', throwDice('D16'))
-console.log('D20:', throwDice('D20'))
+console.log('Несуществующий:', throwDice('D2')) // false
+console.log('Default:', throwDice()) // default dice 1-4
+console.log('D4:', throwDice('D4')) // random dice 1-4
+console.log('D6:', throwDice('D6')) // random dice 1-6
+console.log('D8:', throwDice('D8')) // random dice 1-8
+console.log('D10:', throwDice('D10')) // random dice 1-10
+console.log('D12:', throwDice('D12')) // random dice 1-12
+console.log('D16:', throwDice('D16')) // random dice 1-16
+console.log('D20:', throwDice('D20')) // random dice 1-20
 console.log('--- Конец ---');
